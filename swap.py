1d227a691844ee43f66b5f8f3c779bcb4d07b360
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-04-30 09:32:43 +0600

    Add: swap function to analyze it in terms of time and space

---
+++ swap.py
@@ -0,0 +1,20 @@
+'''
+Given a function.
+Analize it.
+'''
+
+
+def swap(a, b):
+    # TODO
+    # Analize the following statements in terms of time => f(n)
+    # TODO
+    # Analize the following statements in terms of space => s(n)
+    # TODO
+    # Represent them(time, space) in order notation => O()
+    temp = a
+    a = b
+    b = temp
+
+
+if __name__ == '__main__':
+    pass
