0063a6b178160e5ac0b6543d3f0f51c4a98ed371
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-02 10:12:40 +0600

    Add: types of time functions

---
+++ types_of_time_functions.c
@@ -0,0 +1,18 @@
+/*
+ * Types of time functions(classes of functions):
+ *      O(1)    => constant         |   Does not depends on the given input size
+ *      O(logn) => logarithmic      |   Whatever log base is
+ *      O(n)    => linear           |   e.g.: f(n) = 2n+3 | 500n+700 | n/5000+6
+ *      O(n^2)  => quadratic        |   Matrix addition
+ *      O(n^3)  => cubic            |   Matrix multiplication
+ *      O(n^n)  => exponential      |   e.g.: 2^n, 3^n
+ */
+
+
+/*
+ *  Comparison of time function types
+ *      1 < logn < √n < n < nlogn < n^2 < n^3 < ... < 2^n < 3^n < ... < n^n
+ */
+
+// TODO
+// Compare: logn, n, n^2, 2^n with n = 1, 2, 4, 8
