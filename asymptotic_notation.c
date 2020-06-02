28be731cace46106da93a2b552113aab3a198f62
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-02 10:49:27 +0600

    Add: definitions of `Asymptotic notations`

---
+++ asymptotic_notation.c
@@ -0,0 +1,39 @@
+/*
+ * Representing bounds of a function
+ * Asymptotic notations used:
+ *      O   |   big-oh  |   upper bound
+ *      Ω   |   big-omega  |   lower bound
+ *      Θ   |   theta  |   average bound
+ */
+
+
+/* Big-oh def:
+ *      The function `f(n) = O(g(n))` if and only there
+ *      exists `+`(positive) constants `C` and `n0`,
+ *      Such that `f(n) <= C * g(n)` for all `n >= n0`
+*/
+
+// TODO
+// Solve function f(n) = 2n + 3. So that it can be: O(n), O(n^2)
+
+
+// TODO
+// What are the `upper` bounds of f(n) used above?
+// What are the `lower` bounds of f(n) used above?
+
+
+/* Big-omega def:
+ *      The function `f(n) = Ω(g(n))` if and only there
+ *      exists `+`(positive) constants `C` and `n0`,
+ *      Such that `f(n) >= C * g(n)` for all `n >= n0`
+*/
+
+
+/* Big-theta def:
+ *      The function `f(n) = Θ(g(n))` if and only there
+ *      exists `+`(positive) constants `C1`, `C2` and `n0`,
+ *      Such that `C1 * g(n) <= f(n) >= C2 * g(n)`
+*/
+
+// TODO
+// Solve function f(n) = 2n + 3, and find it's theta time class function
