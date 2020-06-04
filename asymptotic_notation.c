c8745b0784d1c0f2a689eba30014ed09fdbc1dc3
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-04 09:38:09 +0600

    Add: examples that dive more into `asymptotic notation`

--- asymptotic_notation.c
+++ asymptotic_notation.c
@@ -37,3 +37,43 @@
 
 // TODO
 // Solve function f(n) = 2n + 3, and find it's theta time class function
+
+
+
+// Given a function:
+//      f(n) = 2n^2 + 3n +4
+
+// TODO
+// Solve for g(n) so that f(n) <= O(g(n^2)).
+// Solve for g(n) so that f(n) >= Ω(g(n^2)).
+
+
+// Given a function:
+//      f(n) = n^2logn + n
+
+// TODO
+// Solve for g(n) so that f(n) <= O(g(n^2logn)).
+// Solve for g(n) so that f(n) >= Ω(g(n^2logn)).
+// Solve for g(n) so that f(n) >= Θ(g(n^2logn)).
+
+
+// The following 2 examples shows the case when you can't find Θ(n)
+// Given a function:
+//      f(n) = n! = n * (n-1) * (n-2) * (n-3) * ... * 3 * 2 * 1
+
+// TODO
+// Prove that lower bound of above function is O(1)
+// Prove that upper bound of above function is O(n^n)
+
+
+//Given a function:
+//      f(n) = logn!
+
+
+// TODO
+// Proving is a little bit tricky:
+//      for lower bound you'll get: log(0)
+//      for lower bound you'll get: log(logn^n)
+// Prove that lower bound of above function is O(1)
+// Prove that upper bound of above function is O(nlogn)
+
