ef6d7b30b071475f3e2fed0de48f4772a502b686
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-11 11:06:35 +0600

    Create, Add: create new folder for algorithmic strategies eg add `linear search` strategy to analize Best/Worst/Average cases

---
+++ strategies/linear_search.c
@@ -0,0 +1,29 @@
+/*
+ * Given an array. Using `linear search` strategy
+ * define its Best/Worst/Average cases.
+ * Remember that it's not Asymptotic notations. Notations are for functions!
+ * Cases(Best, Worst, Average) are just type of analyses done on algorithm.
+ */
+
+ #include <stdio.h>
+
+
+ int nums[] = {8, 6, 12, 5, 9, 7, 4, 3, 16, 18};
+
+// TODO
+//  Find the Best case(searching key element presented at first index)
+//  time in terms of O()
+
+
+// TODO
+//  Find the Worst case(searching key at last index)
+//  time in terms of O()
+
+
+// TODO
+//  Find the Average case(all possible case time / # of cases)
+
+
+int main() {
+
+}
