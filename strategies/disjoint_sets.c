a4d403005f7dd8f0f9398095355bb3dce13235e4
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-15 09:02:45 +0600

    WIP: disjoint sets

---
+++ strategies/disjoint_sets.c
@@ -0,0 +1,11 @@
+/*
+ *  1. Disjoint sets and operations
+ *  2. Detecting a cycle
+ *  3. Graphical representaion
+ *  4. Array reppresentation
+ *  5. Weighted union and collapsing find
+ */
+
+// Given two sets
+int s1[] = {1, 2, 3 ,4}
+int s2[] = {5, 6, 7 ,8}
