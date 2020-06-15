4d820c5026fc7090bc1606a67209d7258e93311d
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-15 07:43:24 +0600

    Add: analyse information of `binary search` algorithm strategy

---
+++ strategies/binary_search.c
@@ -0,0 +1,16 @@
+/*
+ *  INFO
+ *      Best case of `binary search`: search that matches `root elem`.
+ *      Best case time => B(n) = 1
+ *
+ *      Worst case of `binary search`: search for match at `leaf elem`.
+ *      Worst case time => W(n) = h(height)
+ *
+ *      Average case of `binary search`: depends on the length(height) of tree
+ *      Average case time => min W(n) = logn
+ *      Average case time => max W(n) = n
+ */
+
+int main() {
+
+}
