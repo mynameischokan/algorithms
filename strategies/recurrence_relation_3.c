bafcfd14664d67ad3cffef222d9ecc8e59eb8b2c
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-19 11:07:53 +0600

    WIP: reccurence relation direct analyse

---
+++ strategies/recurrence_relation_3.c
@@ -0,0 +1,41 @@
+/*
+ * Write a function that takes `int` as input and if it
+ * is greater than `0` and:
+ *                          1. Initializes `int i=1`, loops till `i<n` with step `i*2`.
+ *                             In this loop prints `n`
+ *                          2. Then calls itself by `-1`.
+ *
+ * Analyse it's time complexity, so that it is class function of `n^2`.
+ */
+
+
+#include <stdio.h>
+
+
+void R1(int n)
+{
+
+}
+
+// TODO
+// What is the T(n)/recurrence relation of R1 function above?
+// Analyse it by taking just degree, remove constants
+
+
+/*
+ *  RECURRENCE RELATION
+ *****************************
+ *            1              n=0
+ *    T(n) =
+ *            T(n-1)+logn    n>0
+ *****************************
+ */
+//  TODO
+//  Solve the Reccurence Realtion using `Recursion Tree`
+//  What is it's time complexity?
+
+
+int main()
+{
+
+}
