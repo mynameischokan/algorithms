f35fcba24ac0c158bfa6506b09b50ef72ce91a8e
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-19 10:13:28 +0600

    Add: recurance relation p1

--- strategies/recurrence_ralation_1.c
+++ strategies/recurrence_ralation_1.c
@@ -1,6 +1,6 @@
 /*
- * Write a function that prints given int if it
- * is greater than 0 and calls itself.
+ * Write a function that prints given `int` if it
+ * is greater than `0` and calls itself by `-1`.
  * Analyse it's time complexity in terms T(n).
  */
 
@@ -39,8 +39,11 @@ int main()
  *      T(n) = T(n-3)+3
  *      T(n) = T(n-k)+k till reach 0(zero)
  *
- *      Assume n-k = 0
+ *      Assume n-k = 0 (was our goal, reach 0(zero). BEcause we know T(n) = 0 => 1)
  *      Then   n = k
  *
  *      T(n) = T(n-n)+n
+ *           = T(0) + n
+ *           = 1 + n
+ *           = O(n)
  */
