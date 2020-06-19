3b73d94aaa3b27c7fc1d602ed52ab5c9c0dd6953
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-19 11:08:28 +0600

    Change: small changes

--- strategies/recurrance_relation_2.c
+++ strategies/recurrance_relation_2.c
@@ -1,11 +1,11 @@
 /*
  * Write a function that takes `int` as input and if it
  * is greater than `0` and:
- *                          1. Initializes `int i = 0`, loops till i < n.
+ *                          1. Initializes `int i = 0`, loops till `i < n`.
  *                             In this loop prints `n`
  *                          2. Then calls itself by `-1`.
  *
- * Analyse it's time complexity.
+ * Analyse it's time complexity, so that it is class function of `n^2`.
  */
 
 
@@ -14,12 +14,7 @@
 
 void R1(int n)
 {
-    if (n > 0) {
-        for (int i=0; i<n; i++) {
-            printf("%d\n", n);
-        }
-        R1(n - 1);
-    }
+
 }
 
 // TODO
