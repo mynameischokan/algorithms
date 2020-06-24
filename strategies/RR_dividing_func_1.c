a89a14cd28de1f41821421b53b8b58d30a554b1c
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-24 12:10:44 +0600

    Add: eg for recurrence relation of dividing function

---
+++ strategies/RR_dividing_func_1.c
@@ -0,0 +1,49 @@
+/*
+ * Write a function that takes `int` as input and if it
+ * is greater than `1` and prints `n`.
+ * Then calls itself  by `n/2`.
+ *
+ * Analyse it's time complexity, so that it is class function of `logn`.
+ */
+
+
+#include <stdio.h>
+
+
+// Recurrence Relation Dividing Function
+void RRDividingF(int n)
+{
+
+};
+
+// TODO
+// What is the RR of function above in terms of T(n)?
+
+
+/*
+ *  RECURRENCE RELATION
+ *****************************
+ *            1                  n=1
+ *    T(n) =
+ *            T(n/2) + 1         n>1
+ *****************************
+ */
+
+//  INFO
+//  a^b = c ====> b = log base(a) c
+
+
+//  TODO
+//  Solve the RR using `Recursion Tree`
+//  so that it will stop at: T(0)
+//  What is it's time complexity?
+
+
+//  TODO
+//  Solve by using `Substitution Method`
+
+
+int main()
+{
+
+}
