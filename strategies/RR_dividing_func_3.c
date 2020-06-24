02d383d1bcb97436a46a4a8dbe98f1884081de24
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-24 12:53:34 +0600

    Add: most important recurrence relation of class `nlogn`

---
+++ strategies/RR_dividing_func_3.c
@@ -0,0 +1,43 @@
+/*
+ * Write a function that takes `int` as input and if it
+ * is greater than `1`(smallest value of division)
+ * and prints `n` for `n` times
+ * Then calls itself twice by `n/2`.
+ *
+ * Analyse it's time complexity, so that it is class function of `nlogn`.
+ */
+
+
+#include <stdio.h>
+
+
+// Recurrence Relation Dividing Function
+void RRDividingF(int n)
+{
+
+};
+
+// TODO
+// What is the RR of function above in terms of T(n)?
+
+
+/*
+ *  RECURRENCE RELATION
+ *****************************
+ *            1                   n=1
+ *    T(n) =
+ *            2T(n/2) + n         n>1
+ *****************************
+ */
+
+
+//  TODO
+//  Solve the Recurrence Relation using `Recursion Tree`
+//  use just time, ignore function T(n) => n,
+//  also take highest term ignore lower ones and constants.
+
+
+int main()
+{
+
+}
