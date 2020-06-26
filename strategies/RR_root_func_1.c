78c31138fdae0e401201cf48f65cd2d25d3820e0
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-26 10:50:35 +0600

    Add: RR eg to be solved for root function

---
+++ strategies/RR_root_func_1.c
@@ -0,0 +1,45 @@
+/*
+ * Write a function that takes `int` as input
+ * and if it is greater than `2` prints `n`.
+ * Then calls itself by `√n`.
+ *
+ * Analyse it's time complexity,
+ * so that it is class function of `loglogn`.
+ */
+
+
+#include <stdio.h>
+
+
+// Recurrence Relation Root Function
+void RRRootF(int n)
+{
+
+};
+
+// TODO
+// What is the RR of function above in terms of T(n)?
+
+
+/*
+ *  RECURRENCE RELATION
+ *****************************
+ *            1                   n=1
+ *    T(n) =
+ *            √n + 1              n>1
+ *****************************
+ */
+
+
+//  HINT
+//  √n == n^1/2
+
+
+//  TODO
+//  Solve the Recurrence Relation using `Substitution Method`
+
+
+int main()
+{
+
+}
