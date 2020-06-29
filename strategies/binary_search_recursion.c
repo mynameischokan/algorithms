9f941a21a21c370f4523b9a48df0fc9cddd6b690
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-29 10:50:35 +0600

    Add: solving problem recursively applying binary search strategy

---
+++ strategies/binary_search_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*  There are two method of search:
+ *      linear
+ *      binary => follows `divide and conquer` strategy
+ *             => list must be already sorted (prerequisite)
+ *             => need two index pointers
+ *             => mid = [(l + h) / 2]
+ */
+
+
+//  Complete the function below using Binary Search recursive method
+int RecursiveBinarySearch(int A[], int n, int key) {
+    /*  A: array of int elements
+        n: length of given array
+        key: element to be searched
+
+        returns: int index of element in array
+    */
+}
+
+
+//  Define it's RR, and what is it's time complexity?
+
+
+int main()
+{
+
+}
