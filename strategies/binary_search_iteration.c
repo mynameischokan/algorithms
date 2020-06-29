ab5ed864434c00a3d189634c59e817183509e5d5
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-06-29 10:39:26 +0600

    Add: binary search strategy iterative method example to be solved

---
+++ strategies/binary_search_iteration.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*  There are two method of search:
+ *      linear
+ *      binary => follows `divide and conquer` strategy
+ *             => list must be already sorted (prerequisite)
+ *             => need two index pointers
+ *             => mid = [(l + h) / 2]
+ */
+
+
+//  Complete the function below using Binary Search iterative method
+int BinarySearch(int A[], int n, int key) {
+    /*  A: array of int elements
+        n: length of given array
+        key: element to be searched
+
+        returns: int index of element in array
+    */
+}
+
+
+//  What is the worst and best cases of function above?
+
+
+int main()
+{
+
+}
